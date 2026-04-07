3b4f6a5d661b6b8881656bf711c405fb6be5b4e1
Clifton King <cliftonk@gmail.com>
2026-04-07 10:18:46 -0500

    Add in-memory metrics helpers (#140)

--- twag/__init__.py
+++ twag/__init__.py
@@ -1,8 +1,12 @@
 """Twitter Aggregator - Market-relevant signal curation from Twitter/X."""
 
+from . import metrics
+
 try:
     from importlib.metadata import version
 
     __version__ = version("twag")
 except Exception:
     __version__ = "0.0.0-dev"
+
+__all__ = ["__version__", "metrics"]
